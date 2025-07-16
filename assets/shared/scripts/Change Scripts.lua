e6cfc0531e1e144973d69794c33148e1f7e76401
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-17 01:31:58 +0200

    unzipgit

---
+++ assets/shared/scripts/Change Scripts.lua
@@ -0,0 +1,77 @@
+function onCreate()
+if songName == "Child's Play" or songName == 'Retcon' then 
+addLuaScript('scripts extras/HealthBar Gumball') 
+addLuaScript('scripts extras/Apocalypse Count Gumball')
+
+if getPropertyFromClass('ClientPrefs', 'shaders') == true then
+addLuaScript('scripts extras/Shaders/Apocalypse shader')
+addLuaScript('scripts extras/Shaders/glitchy notes by ansfoxy')
+addLuaScript('scripts extras/Shaders/Glitchy icon') 
+end
+end
+
+if songName == 'My Amazing World' then 
+addLuaScript('scripts extras/HealthBar Gumball') 
+addLuaScript('scripts extras/Apocalypse Count Gumball')
+
+if getPropertyFromClass('ClientPrefs', 'shaders') == true then
+addLuaScript('scripts extras/Shaders/Apocalypse shader')
+addLuaScript('scripts extras/Shaders/glitchy notes by ansfoxy')
+addLuaScript('scripts extras/Shaders/Glitchy icon') 
+addLuaScript('scripts extras/Shaders/TvGumball') 
+end
+end
+
+if songName == 'Forgotten World' then
+addLuaScript('scripts extras/Apocalypse Count Gumball')
+
+if getPropertyFromClass('ClientPrefs', 'shaders') == true then
+addLuaScript('scripts extras/Shaders/Apocalypse shader')
+addLuaScript('scripts extras/Shaders/glitchy notes by ansfoxy')
+addLuaScript('scripts extras/Shaders/Glitchy icon') 
+end
+end
+
+if songName == 'Mindless' or songName == 'Blessed by Swords' or songName == 'Brotherly Love' then 
+addLuaScript('scripts extras/HealthBar Finn') 
+addLuaScript('scripts extras/Apocalypse Count Finn')
+
+if getPropertyFromClass('ClientPrefs', 'shaders') == true then
+addLuaScript('scripts extras/Shaders/Apocalypse shader')
+addLuaScript('scripts extras/Shaders/glitchy notes by ansfoxy')
+addLuaScript('scripts extras/Shaders/Glitchy icon') 
+end
+end
+
+if songName == 'Suffering Siblings' or songName == 'Suffering Siblings V3' then
+addLuaScript('scripts extras/HealthBar Finn') 
+addLuaScript('scripts extras/Apocalypse Count Finn')
+
+if getPropertyFromClass('ClientPrefs', 'shaders') == true then
+addLuaScript('scripts extras/Shaders/Apocalypse shader')
+addLuaScript('scripts extras/Shaders/glitchy notes by ansfoxy')
+--addLuaScript('scripts extras/Shaders/Glitchy icon') 
+addLuaScript('scripts extras/Shaders/Glitchy icon2') 
+end
+end
+
+if songName == "Come Along With Me" then 
+if getPropertyFromClass('ClientPrefs', 'shaders') == true then
+addLuaScript('scripts extras/Shaders/Reveal shader')
+addLuaScript('scripts extras/Shaders/Finn glitchy')
+addLuaScript('scripts extras/Shaders/glitchy notes by ansfoxy')
+end
+end
+
+if songName == "No Hero Remix" then 
+addLuaScript('scripts extras/HealthBar Gumball') 
+addLuaScript('scripts extras/Apocalypse Count Gumball')
+
+if getPropertyFromClass('ClientPrefs', 'shaders') == true then
+addLuaScript('scripts extras/Shaders/Apocalypse shader')
+addLuaScript('scripts extras/Shaders/glitchy notes by ansfoxy')
+addLuaScript('scripts extras/Shaders/Glitchy icon') 
+end
+end
+
+end
