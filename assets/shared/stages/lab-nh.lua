6c9bbc959f065c5bdf515a9126f3b97e5cc404e2
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-17 12:29:37 +0200

    unzipgit

--- assets/shared/stages/lab-nh.lua
+++ assets/shared/stages/lab-nh.lua
@@ -1,13 +1,9 @@
 function onCreate()
 makeLuaSprite('BG0', 'stages/lab-nh/bgnoherofull', -800, -300);
 scaleObject('BG0', 2, 2);
-doTweenX('BG0X', 'BG0.scale', 2, 0.001, 'linear')
-doTweenY('BG0Y', 'BG0.scale', 2, 0.001, 'linear')
 
 makeLuaSprite('BG1', 'stages/lab-nh/place', -800, -300);
 scaleObject('BG1', 2, 2);
-doTweenX('BG1X', 'BG1.scale', 2, 0.001, 'linear')
-doTweenY('BG1Y', 'BG1.scale', 2, 0.001, 'linear')
 
 makeLuaSprite("white","white", 0, 0);
 setObjectCamera("white", 'other')
@@ -22,31 +18,55 @@ setObjectCamera("black2", 'game')
 doTweenColor('black2C', 'black2', 'FFFFFF', 0.01, 'linear')
 doTweenAlpha("black2A","black2", 0, 0.1, true);
 
-makeAnimatedLuaSprite('Cutt','noherocutscenefirst', -200, -400);
+makeAnimatedLuaSprite('Cutt','noherocutscenefirst', -350, -500);
 luaSpriteAddAnimationByPrefix('Cutt', 'hola', 'play', 24, true);
-scaleObject('Cutt', 2, 2);
-doTweenX('CuttX', 'Cutt.scale', 2.4, 0.001, 'linear')
-doTweenY('CuttY', 'Cutt.scale', 2.4, 0.001, 'linear')
+scaleObject('Cutt', 2.4, 2.4);
 setObjectCamera('Cutt', 'other')
 doTweenAlpha("CuttA","Cutt", 0, 0.1, true);
-
-makeLuaSprite('c', 'mov', 0, 0);
-setObjectCamera('c', 'hud');
-doTweenX('cX', 'c.scale', 2, 0.001, 'linear')
-doTweenY('cY', 'c.scale', 2, 0.001, 'linear')
 
 makeLuaSprite("black","blacktnt", 0, 0);
 setProperty("black.scale.x", 5380);
 setProperty("black.scale.y", 5380);
 setObjectCamera("black", 'other')
 
+makeLuaSprite("blackFU","blacktnt", 680, 400);
+setProperty("blackFU.scale.x", 1380);
+setProperty("blackFU.scale.y", 820);
+setObjectCamera("blackFU", 'other')
+
+makeLuaSprite('Move1',nil, 0, 0)
+setObjectCamera('Move1','hud')
+makeGraphic('Move1', screenWidth, 100, '000000')
+doTweenY('Move1Y', 'Move1', -110, 0.001, 'sineInOut');
+
+makeLuaSprite('Move2',nil, 0, 620)
+setObjectCamera('Move2','hud')
+makeGraphic('Move2', screenWidth, 100, '000000')
+doTweenY('Move2Y', 'Move2', 720, 0.001, 'sineInOut');
+
+makeLuaSprite('Move1B',nil, 0, 0)
+setObjectCamera('Move1B','other')
+makeGraphic('Move1B', screenWidth, 100, '000000')
+doTweenY('Move1BY', 'Move1B', 0, 0.001, 'sineInOut');
+doTweenAlpha("Move1AlB","Move1B", 0, 0.1, 'linear');
+
+makeLuaSprite('Move2B',nil, 0, 620)
+setObjectCamera('Move2B','other')
+makeGraphic('Move2B', screenWidth, 100, '000000')
+doTweenY('Move2BY', 'Move2B', 620, 0.001, 'sineInOut');
+doTweenAlpha("Move2AlB","Move2B", 0, 0.1, 'linear');
+
+addLuaSprite('Move1',false)
+addLuaSprite('Move2',false)
+addLuaSprite('Move1B',false)
+addLuaSprite('Move2B',false)
 addLuaSprite('BG0', false);
 addLuaSprite('BG1', false);
 addLuaSprite("black", true);
 addLuaSprite("black2", false);
+addLuaSprite("blackFU", false);
 addLuaSprite('Cutt', false)
 addLuaSprite("white", false);
-addLuaSprite('c', false);
 
 setProperty('BG0.visible', true)
 setProperty('BG1.visible', false)
@@ -57,14 +77,14 @@ doTweenAlpha("CuttA","Cutt", 1, 0.1, true);
 luaSpritePlayAnimation('Cutt', 'hola', true);
 triggerEvent('Ansfoxy', 'inicio', '')
 doTweenAlpha("black2","black", 0, 0.01, 'linear');
+doTweenColor('icon1', 'iconP2', '000000', 0.1, 'linear')
 end
 
 function onEvent(name,value1,value2)
 if name == 'Ansfoxy' then 
 if value1 == 'inicio' then
-
-doTweenX('cX', 'c.scale', 1, 0.001, 'linear')
-doTweenY('cY', 'c.scale', 1, 0.001, 'linear')
+doTweenY('Move1Y', 'Move1', 0, 0.6, 'sineInOut');
+doTweenY('Move2Y', 'Move2', 620, 0.6, 'sineInOut');
 doTweenAlpha("white1","white", 5, 000000.1, 'linear');
 doTweenAlpha("white2","white", 0, 1, 'linear');
 doTweenAlpha("black2A","black2", 1, 0.1, true);
@@ -79,8 +99,16 @@ doTweenAlpha("CuttA","Cutt", 0, 0.1, true);
 end
 
 if value1 == 'inicio2' then
-doTweenX('cX', 'c.scale', 2, 0.001, 'linear')
-doTweenY('cY', 'c.scale', 2, 0.001, 'linear')
+doTweenAlpha('fadeInn', 'camHUD', 1, 0.5, 'linear')
+doTweenColor('icon1', 'iconP2', 'ffffff', 0.1, 'linear')
+doTweenY('Move1Y', 'Move1', -110, 0.6, 'sineInOut');
+doTweenY('Move2Y', 'Move2', 720, 0.6, 'sineInOut');
+doTweenY('Move1BY', 'Move1B', -110, 0.6, 'sineInOut');
+doTweenY('Move2BY', 'Move2B', 720, 0.6, 'sineInOut');
+doTweenAlpha("Move1Al","Move1", 1, 0.5, 'linear');
+doTweenAlpha("Move2Al","Move2", 1, 0.5, 'linear');
+doTweenAlpha("Move1AlB","Move1B", 0, 0.5, 'linear');
+doTweenAlpha("Move2AlB","Move2B", 0, 0.5, 'linear');
 doTweenAlpha("white1","white", 5, 000000.1, 'linear');
 doTweenAlpha("white2","white", 0, 1, 'linear');
 doTweenAlpha("black2A","black2", 0, 0.1, true);
@@ -92,6 +120,14 @@ end
 
 if value1 == 'hud' then
 doTweenAlpha('fadeInn', 'camHUD', 0, 0.5, 'linear')
+end
+
+if value1 == 'hud2' then
+doTweenAlpha('fadeInn', 'camHUD', 0, 0.5, 'linear')
+doTweenAlpha("Move1Al","Move1", 0, 0.5, 'linear');
+doTweenAlpha("Move2Al","Move2", 0, 0.5, 'linear');
+doTweenAlpha("Move1AlB","Move1B", 1, 0.5, 'linear');
+doTweenAlpha("Move2AlB","Move2B", 1, 0.5, 'linear');
 end
 
 if value1 == 'stage1' then
