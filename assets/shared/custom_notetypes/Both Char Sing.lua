213c3910172cb5ec5b59e259c803a021257d1a23
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-17 12:30:13 +0200

    unzipgit

--- assets/shared/custom_notetypes/Both Char Sing.lua
+++ assets/shared/custom_notetypes/Both Char Sing.lua
@@ -4,6 +4,7 @@ function onCreate()
 		--Check if the note is an Instakill Note
 		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Both Char Sing' then
 				setPropertyFromGroup('unspawnNotes', i, 'ignoreNote', false); --Miss has penalties
+
 			end
 		end
 	end
