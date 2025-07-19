6e834a3262ae3eeccad1813e05fc357c1b733bf4
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-19 14:08:40 +0200

    unzipgit

--- assets/shared/custom_notetypes/Dodge Note.lua
+++ assets/shared/custom_notetypes/Dodge Note.lua
@@ -1,41 +1,12 @@
-function onCreate()
-for i = 0, getProperty('unspawnNotes.length')-1 do
-if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Dodge Note' then
-setPropertyFromGroup('unspawnNotes', i, 'noAnimation', true); 
-end
-end
-end
-
-function goodNoteHit(id, direction, noteType, isSustainNote)
-if noteType == 'Dodge Note' then
-if songName == 'Suffering Siblings' then 
-       anim = ''
-       setProperty('boyfriend.specialAnim', true);
-                if direction == 0 then
-                    anim = 'singLEFT'
-                elseif direction == 1 then
-                    anim = 'singDOWN'
-                elseif direction == 2 then
-                    anim = 'singUP'
-                elseif direction == 3 then
-                    anim = 'singRIGHT'
-                end
-                triggerEvent('Play Animation', anim, 'boyfriend');
-    else
-    
-       anim = ''
-       setProperty('boyfriend.specialAnim', true);
-                if direction == 0 then
-                    anim = 'dodgeLEFT'
-                elseif direction == 1 then
-                    anim = 'dodgeDOWN'
-                elseif direction == 2 then
-                    anim = 'dodgeUP'
-                elseif direction == 3 then
-                    anim = 'dodgeRIGHT'
-                end
-                triggerEvent('Play Animation', anim, 'boyfriend');
-    end
-end
-end
-
+local dodgeName = {
+	{'dodgeLEFT'},
+	{'dodgeDOWN'},
+	{'dodgeUP'},
+	{'dodgeRIGHT'},
+	}
+function goodNoteHit(id,dir,type,sus)
+if type == 'Dodge Note' then
+		characterPlayAnim('boyfriend',dodgeName[dir+ 1][1] , true)
+		setProperty('boyfriend.specialAnim', true);
+	end
+	end
