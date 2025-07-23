86ab7bf0b450a8306d626c9ae9e848bb37b2c325
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-24 00:25:00 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -44,7 +44,8 @@ class Main extends Sprite
 	};
 
 	public static var fpsVar:FPSCounter;
-
+        
+	public static final PSYCH_ENGINE_VERSION:String = "0.71";
 	public static final platform:String = #if mobile "Phones" #else "PCs" #end;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
