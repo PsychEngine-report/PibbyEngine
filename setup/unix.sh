c3bf338e2b309f68a73035e154fdb52963d87fb6
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-12-11 20:02:12 +0100

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -7,10 +7,10 @@ echo Makking the main haxelib and setuping folder in same time..
 mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib git hxcpp https://github.com/kittycathy233/hxcpp --quiet
-haxelib git lime https://github.com/kittycathy233/lime --quiet
+haxelib git hxcpp https://github.com/AlafandyPorting/hxcpp --quiet
+haxelib git lime https://github.com/HomuHomu833-haxe-stuff/lime --quiet
 haxelib install openfl 9.4.1 --quiet
-haxelib git flixel https://github.com/kittycathy233/flixel --quiet
+haxelib git flixel https://github.com/Prohack101010/flixel-5.8.0-fixed --quiet
 haxelib install flixel-addons 3.3.2 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
 haxelib install hscript-iris 1.1.3 --quiet
@@ -18,7 +18,8 @@ haxelib install tjson 1.4.0 --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
 haxelib git linc_luajit https://github.com/kittycathy233/linc_luajit --quiet
 haxelib install hxdiscord_rpc --quiet --skip-dependencies
-haxelib install hxvlc 2.0.1 --quiet --skip-dependencies
+haxelib install hxvlc --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
+haxelib git mobile-controls https://github.com/Prohack101010/mobile-controls-dev --quiet //credit :D https://github.com/Prohack101010
 echo Finished!
