9caad543ac22dc72e0ae3bfee0baf2076fb1f810
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-12-13 23:25:07 +0100

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -87,7 +87,17 @@ class TitleState extends MusicBeatState
 			}
 			persistentUpdate = true;
 			persistentDraw = true;
-			MobileData.init();
+			MobileConfig.init('MobileControls', CoolUtil.getSavePath(), 'assets/mobile/',
+				[
+					'MobilePad/DPadModes',
+					'MobilePad/ActionModes',
+					'Hitbox/HitboxModes',
+				], [
+					DPAD,
+					ACTION,
+					HITBOX
+				]
+			);
 		}
 
 		if (FlxG.save.data.weekCompleted != null)
