e4a0b0aae45e56bd036b96ca44ef7e931873f094
Ethantobot <ethantobot7@gmail.com>
2026-03-26 18:30:08 +0100

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -14,24 +14,19 @@ import llua.Lua;
 import backend.Achievements;
 #end
 
+#if MOBILE_CONTROLS_ALLOWED
 import mobile.*;
-import mobile.backend.StorageUtil;
-import mobile.objects.IMobileControls;
-import mobile.input.MobileInputManager;
-//import mobile.substates.MobileExtraControl;
-//Android
+import mobile.objects.*;
+import mobile.MobileConfig.ButtonModes;
+#end
+
+#if mobile
+import mobile.MobileControlManager;
+import mobile.StorageUtil;
+import mobile.ScreenUtil;
+#end
+
 #if android
-#if legacy_lime
-import extension.androidtools.callback.CallBack as AndroidCallBack;
-import extension.androidtools.content.Context as AndroidContext;
-import extension.androidtools.widget.Toast as AndroidToast;
-import extension.androidtools.os.Environment as AndroidEnvironment;
-import extension.androidtools.Permissions as AndroidPermissions;
-import extension.androidtools.Settings as AndroidSettings;
-import extension.androidtools.Tools as AndroidTools;
-import extension.androidtools.os.Build.VERSION as AndroidVersion;
-import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
-#else
 import android.callback.CallBack as AndroidCallBack;
 import android.content.Context as AndroidContext;
 import android.widget.Toast as AndroidToast;
@@ -41,7 +36,6 @@ import android.Settings as AndroidSettings;
 import android.Tools as AndroidTools;
 import android.os.Build.VERSION as AndroidVersion;
 import android.os.Build.VERSION_CODES as AndroidVersionCode;
-#end
 #end
 
 #if sys
@@ -63,7 +57,6 @@ import backend.BaseStage;
 import backend.Difficulty;
 import backend.Mods;
 import backend.Language;
-import mobile.backend.StorageUtil;
 
 import backend.ui.*; //Psych-UI
 
